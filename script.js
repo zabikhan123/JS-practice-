29d7ca3bcce4e0f676893d2a5f29f2dd4a3f7a1c
zabi <zabikhan5567@gmail.com>
2025-08-23 21:44:23 -0700

    use while loop

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-// loops in js
-for (let i = 1 ; i <= 10 ; i++){
-    console.log("hello world")
+// loops in js  3 luctire
+
+
+
+// for (let i = 1 ; i <= 10 ; i++){
+//     console.log("hello world")
+// }
+// let sum = 0;
+// for (let i =1; i <= 10 ; i++){
+//     sum = sum +i;
+// }
+// console.log("sum =", sum);
+// let sum = 10;
+// let n = 100;
+// for (let i = 1 ; i <= n ; i++){
+//     sum = sum +i ;
+// }
+// console.log("sum =", sum);
+
+// while loop in js
+
+// let i = 1;
+// while( i <= 10){
+//     console.log(" i =", i);
+//     i++;
+// }
+
+let i = 1;
+while( i <= 20){
+    console.log(" i =", i);
+    i++;
 }
